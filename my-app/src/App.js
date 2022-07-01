9bf202273e4a267f1c41e3dd95cf06545f21ea48
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-07-02 01:05:12 +0700

    update all

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom/client';
+// import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {dishes:DISHES}
+  }
+  render(){
+    return (
+      <div className="App" id ="root">
+        <Navbar dark color="primary"> 
+          <div className="container">
+            <NavbarBrand href="/"> Hello hihi</NavbarBrand>
+          </div>
+        </Navbar>
+         <Menu dishes={this.state.dishes} />
+        
+      </div>
+      
+    );
+  }
+}
+// function App() {
+  
+// }
+
+export default App;
