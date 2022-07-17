026b6a7335e13ba41209c21f229ecadbb7e9a5a6
HienFX14975 <hientnfx14975@funix.edu.vn>
2022-07-18 02:53:52 +0700

    thiet lap dau tien

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client';
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import { StaffList } from './components/StaffListComponent';
+// import { DISHES } from './shared/dishes';
+import { STAFFS } from './shared/staffs';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {dishes:DISHES}
+    this.state = {staffs:STAFFS}
   }
   render(){
     return (
       <div className="App" id ="root">
         <Navbar dark color="primary"> 
           <div className="container">
-            <NavbarBrand href="/"> Hello hihi</NavbarBrand>
+            <NavbarBrand href="/"> Nhân viên</NavbarBrand>
           </div>
         </Navbar>
-         <Menu dishes={this.state.dishes} />
+         {/* <StaffList staffs={this.state.staffs} /> */}
       </div>
     );
   }
